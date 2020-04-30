c03a2f92812931308f0eaa9b2ca68a16ea4279c1
enzonox <enzod.leca@gmail.com>
2020-04-30 16:57:44 -0300

    Projeto feito no Fulltack Lab

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='py-4 text-center bg-gray-400'>
+            MyDailyStatus é um projeto criado durante o Fullstack Lab do DevPeno
+            <br/>
+            Implementado por:{' '} 
+            <a href='https://linkedin.com/in/enzoleça/'>Enzo Leça</a>
+        </div>
+    )
+}
+
+export default Footer
---
+++ components/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Link from 'next/link';
+
+//esse componente recebe a propriedade href com o nome do link que é o children
+const NavLink = ({ href, children }) => {
+    return (
+        <Link href={href}>
+            <a className='p-2 hover:underline hover:text-red-800'>{children}</a>
+        </Link>
+    )
+}
+
+const NavBar = () => {
+    return (
+        <div className="bg-gray-500 py-4 text-center">
+            <NavLink href="/sobre">Sobre</NavLink>
+            <NavLink href="/cadastro">Cadastro</NavLink>
+            <NavLink href="/entrar">Entrar</NavLink>
+        </div>
+    )
+}
+
+export default NavBar
